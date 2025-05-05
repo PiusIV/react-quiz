78a81d7c40e1f75e96b04f89a83505288ed84a74
PiusCyre <108563817+PiusCyre@users.noreply.github.com>
2025-05-05 13:31:46 +0100

    modified script in package.json, for api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import logo from "../assets/react.svg";
+
 function Header() {
   return (
     <header className="flex gap-6 justify-center text-3xl font-mini font-bold mb-8">
-      <img src="/public/react.svg" alt="React logo" className="" />
+      <img src={logo} alt="React logo" className="" />
       <h1>The React Quiz</h1>
     </header>
   );
