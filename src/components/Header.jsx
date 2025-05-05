77e8f5c8b0d96100a5152b50fa756fba14941344
PiusCyre <108563817+PiusCyre@users.noreply.github.com>
2025-05-05 05:11:14 +0100

    m src

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="flex gap-6 justify-center text-3xl font-mini font-bold mb-8">
-      <img src="/src/assets/react.svg" alt="React logo" className="" />
+      <img src="/public/react.svg" alt="React logo" className="" />
       <h1>The React Quiz</h1>
     </header>
   );
